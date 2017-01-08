4c6060fe262e8e740772e9fae79988ee4cac9af1
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-01-08 21:48:08 +0100

    add custom mLab login for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
-
+if (!process.env.MONGODB_URI) {
+	var {mongodbLogin} = require('./mongolab-login');
+	console.log(mongodbLogin);
+}
 // promise library to use
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || mongodbLogin || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose
