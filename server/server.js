bcf657e044d1563234333987e66d0bfd430490d8
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-01-08 21:19:42 +0100

    Add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 // converts json into object
 const bodyParser = require('body-parser');
@@ -68,6 +69,32 @@ app.delete('/todos/:id', (req,res) => {
 		res.status(400).send();
 	});
 });
+
+app.patch('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	// whitelisting params
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((err) => {
+		res.status(400).send();
+	})
+})
 
 app.listen(port, () => {
 	console.log(`Server up at port ${port}`);
