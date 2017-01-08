520603e4ce36a994486481ed2695fe29ce7a1cdb
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-01-08 16:36:48 +0100

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // converts json into object
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,26 @@ app.get('/todos', (req,res) => {
 	}, (err) => {
 		res.status(400).send(err);
 	});
+});
+
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+			// return console.log('Todo not found');
+		}
+		res.send({todo});
+	}).catch((err) => {
+		res.status(400).send();
+		// console.log(err);
+	});
+
 });
 
 app.listen(3000, () => {
