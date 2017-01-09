250dd4bd76895a7f70008e3094337c4d1ca8ad5c
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-01-09 16:58:30 +0100

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -35,6 +35,7 @@ var UserSchema = new Schema({
 	}]
 });
 
+// instance methods
 UserSchema.methods.toJSON = function () {
 	var user = this;
 	var userObject = user.toObject();
@@ -54,6 +55,27 @@ UserSchema.methods.generateAuthToken = function () {
 
 	return user.save().then(() => {
 		return token;
+	});
+};
+
+// model method
+UserSchema.statics.findByToken = function (token) {
+	var User = this;
+	var decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		// return new Promise((resolve, reject) => {
+		// 	reject();
+		// });
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
 	});
 };
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -111,6 +112,10 @@ app.post('/users', (req,res) => {
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/users/me', authenticate, (req,res) => {
+	res.send(req.user);
 });
 
 
